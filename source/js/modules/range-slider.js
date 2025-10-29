1733bd32b5c073b71e9e2dd81921f2246208381e
Anastasiia Tyustina <tyustina.av@yandex.ru>
2025-10-30 01:32:37 +0500

    Update range-slider.js

--- source/js/modules/range-slider.js
+++ source/js/modules/range-slider.js
@@ -1,3 +1,20 @@
+import noUiSlider from '../vendor/nouislider.mjs';
+const form = document.querySelector('.form')
+export function createRangeSlider() {
+    if (form) {
+        const rangeSlider = document.getElementById('range-slider');
+        console.log('все работает');
+
+        noUiSlider.create(rangeSlider, {
+            start: [30, 10],
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 100
+            },
+            step: 1,
+            format: {
+                to: value => Math.round(value),
                 from: value => Number(value)
             }
         });
